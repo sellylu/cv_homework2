d753b2cf6055b72acd6d09f44064f0296f989364
sellylu <s83721456@gmail.com>
2016-10-26 21:23:54 +0800

    finish RANSAC

--- code/RANSACFit.m
+++ code/RANSACFit.m
@@ -97,8 +97,10 @@ function dists = ComputeError(H, pt1, pt2, match)
     % matrix whose first row is pt1(match(1,1),:), second row is 
     % pt1(match(2,1),:), etc. (You may use 'for' loops if this is too
     % confusing, but understanding it will make your code simple and fast.)
-    dists = zeros(size(match,1),1);
-
+    %dists = zeros(size(match,1),1);
+	pt1_match = [pt1(match(:,1),:) ones(length(match),1)]*H';
+	pt2_match = pt2(match(:,2),:)
+	dists = sqrt(sum((pt1_match(:,1:2) - pt2_match).^2,2));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                              %
 %                                 END YOUR CODE                                %
